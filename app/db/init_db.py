b048f28c1bd817f064ac0e10f76b08868cda8356
Simon Suarez <simon.suarez.ovalle@gmail.com>
2026-01-20 12:12:12 -0300

    fix: se agrega el import del modelo para poder crear la tabla usuario

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,4 +1,5 @@
 from db.base import Base
 from db.conexionBD import engine
+import models.user_model  # Asegura que los modelos se importen para crear las tablas
 
 Base.metadata.create_all(bind=engine)
